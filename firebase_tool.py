6380a1b0c0eb42586ed85e322598bb600077b416
Sylvain Willy <willy.sylvain@gmail.com>
2020-05-20 19:00:50 +0200

    Add compiled protobuf files

---
+++ firebase_tool.py
@@ -0,0 +1,27 @@
+import json
+import os
+import time
+
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
+
+
+class FirebaseTool:
+    def __init__(self):
+        service_account_key = os.environ.get("FIREBASE_CERTIFICATE", None)
+        if service_account_key is None:
+            cred = credentials.Certificate("serviceAccountKey.json")
+        else:
+            cred = credentials.Certificate(json.loads(service_account_key))
+        self.firebase_app = firebase_admin.initialize_app(cred, {
+            'storageBucket': 'hidehunt-71e41.appspot.com'
+        })
+        self.db = firestore.client()
+        self.bucket = storage.bucket()
+
+    def start_game(self, game_id: str, action: str):
+        self.db.collection(u'game_queue').add({
+            u'timestamp': time.time(),
+            u'action': action,
+            u'game_id': game_id
+        })
