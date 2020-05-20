1e48d0d57eb1b758a07f19b918bcdf8e0b7c27de
Sylvain Willy <willy.sylvain@gmail.com>
2020-05-20 16:37:28 +0200

    Add FIREBASE_CERTIFICATE env variable as credentials source

--- mqtt_helper.py
+++ mqtt_helper.py
@@ -1,11 +1,16 @@
 import json
+import os
 
 import paho.mqtt.client as mqtt
 
 
 def connect_mqtt_with_credentials(client: mqtt.Client):
-    with open('mqtt_settings.json') as json_settings_file:
-        settings = json.load(json_settings_file)
+    settings_env = os.environ.get("FIREBASE_CERTIFICATE", None)
+    if settings_env is not None:
+        settings = json.loads(settings_env)
+    else:
+        with open('mqtt_settings.json') as json_settings_file:
+            settings = json.load(json_settings_file)
 
     client.username_pw_set(settings["username"], settings["password"])
     client.connect(settings["server_url"], settings["port"])
